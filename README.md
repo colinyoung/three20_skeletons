05aded1eb44e5426fe9b6c74541bf863d06cf7fa
Colin Young <me@colinyoung.com>
2011-08-17 17:22:12 -0500

    initial.

---
+++ README.md
@@ -0,0 +1,1 @@
+Skeletons for three20.
---
+++ TTTableViewDataSource.m
@@ -0,0 +1,55 @@
+#pragma mark - TTTableViewDataSource protocol delegate
+
+- (id)tableView:(UITableView*)tableView objectForRowAtIndexPath:(NSIndexPath*)indexPath{ 
+    return nil; 
+}
+
+- (Class)tableView:(UITableView*)tableView cellClassForObject:(id)object{
+    return nil; 
+}
+
+- (NSString*)tableView:(UITableView*)tableView labelForObject:(id)object{ 
+    return nil; 
+}
+
+- (NSIndexPath*)tableView:(UITableView*)tableView indexPathForObject:(id)object{ 
+    // return (NSIndexPath*)indexPath;
+    return nil;
+}
+
+- (void)tableView:(UITableView*)tableView cell:(UITableViewCell*)cell
+willAppearAtIndexPath:(NSIndexPath*)indexPath{ 
+    // implementation here
+}
+
+- (void)tableViewDidLoadModel:(UITableView*)tableView{ 
+    // implementation here
+}
+
+- (NSString*)titleForLoading:(BOOL)reloading{ 
+    return reloading ? @"Loading..." : @"Done."; 
+}
+
+- (UIImage*)imageForEmpty{ 
+    return nil; 
+}
+
+- (NSString*)titleForEmpty{ return @""; 
+}
+
+- (NSString*)subtitleForEmpty{ return @"No data."; }
+
+- (UIImage*)imageForError:(NSError*)error{ return nil; }
+
+- (NSString*)titleForError:(NSError*)error{ return @"Error"; }
+
+- (NSString*)subtitleForError:(NSError*)error{ return @"Sorry, there was an error.; }
+
+#pragma mark - UITableViewDataSource protocol delegate
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+-(int)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
