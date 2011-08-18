5fb53d605a26fe77944bc507689d6a7af48b3f60
Colin Young <me@colinyoung.com>
2011-08-18 15:22:33 -0500

    Added TTTableViewDelegate

---
+++ TTTableViewDelegate.m
@@ -0,0 +1,6 @@
+- (void)tableView:(UITableView*)tableView touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event {
+#warning tableView:touchesBegan:withEvent needs implementation
+}
+- (void)tableView:(UITableView*)tableView touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event {
+#warning tableView:touchesBegan:withEvent needs implementation	
+}
